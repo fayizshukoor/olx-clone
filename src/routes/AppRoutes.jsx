f518b80e7abd99cf4a25faf99420a78748da7005
fayizshukoor <fayizshukoorkk@gmail.com>
2026-08-11 21:06:44 +0530

    Initial commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Home from '../pages/Home';
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <App/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path:'login',
+                element: <Login/>
+            },
+            {
+                path:'signup',
+                element: <Signup/>
+            }
+        ]
+    }
+]);
+
+export default router;
